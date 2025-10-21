2c35a16224c062ee3a3db9581973c03575c7eca4
yuetsh <517252939@qq.com>
2025-10-21 21:43:18 +0800

    fix

--- pg_logger.py
+++ pg_logger.py
@@ -75,7 +75,7 @@ PYTUTOR_HIDE_STR = "#pythontutor_hide:"
 # - also accepts shell globs, just like PYTUTOR_HIDE_STR
 PYTUTOR_INLINE_TYPE_STR = "#pythontutor_hide_type:"
 
-CLASS_RE = re.compile("class\s+")
+CLASS_RE = re.compile(r"class\s+")
 
 
 # copied-pasted from translate() in https://github.com/python/cpython/blob/2.7/Lib/fnmatch.py
@@ -113,7 +113,7 @@ def globToRegex(pat):
                 res = "%s[%s]" % (res, stuff)
         else:
             res = res + re.escape(c)
-    return res + "\Z(?ms)"
+    return res + r"\Z(?ms)"
 
 
 def compileGlobMatch(pattern):
